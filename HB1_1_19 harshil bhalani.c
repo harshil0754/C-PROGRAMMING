3191ec91a89c5affbb801f2f57e3b49988379716
harshil0754 <harshilbhalani4570@gmail.com>
2025-09-04 00:35:48 +0530

    Add files via upload

---
+++ HB1_1_19 harshil bhalani.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area;
+
+
+    printf("Enter the radius of the circle ");
+    scanf("%f", &radius);
+
+
+    area = (22.0 / 7.0) * radius * radius;
+
+
+    printf(" finded Area of the circle is %.2f\n", area);
+
+    return 0;
+}
+
+
